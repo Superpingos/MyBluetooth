ce99523cdfcf6c68cb38168bf8ddf713ac894b98
Superpingos <superpingos@yahoo.com>
2017-09-23 15:41:01 +0200

    Initial Commit

---
+++ MyBluetooth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyBluetooth
+//
+//  Created by Steve NICOUD on 23.09.17.
+//  Copyright © 2017 Superpingos. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
